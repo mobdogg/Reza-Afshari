64e534a89249e6f72661462e6395bd4820169154
mobdogg <58931842+mobdogg@users.noreply.github.com>
2020-05-13 21:59:10 +0700

    Create hosta.py

---
+++ .github/workflows/hosta.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import paramiko
+import tarfile
+import os
+
+os.system("docker run -d --name looper busybox:latest /bin/sh -c 'i=0; while true; do echo $i; i=$(expr $i + 1); sleep 1; done'")
+os.system("docker checkpoint create --checkpoint-dir=/migration looper checkpoint1")
+
+def tardir(path, tar_name):
+    with tarfile.open(tar_name, "w:gz") as tar_handle:
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                tar_handle.add(os.path.join(root, file))
+tardir('/migration', 'checkpoint1.tgz')
+
+ssh=paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(hostname="toshiba",username="toshiba",password="123456")
+#Raises BadHostKeyException,AuthenticationException,SSHException,socket error
+
+sftp=ssh.open_sftp()
+sftp.put('/migration/checkpoint1.tgz','/migration/checkpoint1.tgz')
