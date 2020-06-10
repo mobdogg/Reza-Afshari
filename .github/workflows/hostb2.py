326e7702320a4bfd3475ac872167d5a7d8e94051
mobdogg <58931842+mobdogg@users.noreply.github.com>
2020-06-10 20:06:12 +0700

    Update hostb2.py

--- .github/workflows/hostb2.py
+++ .github/workflows/hostb2.py
@@ -1,14 +1,18 @@
 import os
 import socket
+import sys
+from datetime import datetime 
 
-
-os.system("docker create --name looper-clone busybox:latest \ /bin/sh -c 'i=0; while true; do echo $i; i=$(expr $i + 1); sleep 1; done'")
+os.system("docker create --name looper-clone oraclelinux:8.2 \ /bin/sh -c 'i=0; while true; do echo $i; i=$(expr $i + 1); sleep 1; done'")
 os.system("docker start --checkpoint-dir=/migration/migration --checkpoint=checkpoint1 looper-clone")
+sys.stdout.write("Checkpoint Restored ")
+print datetime.now().strftime('%H:%M:%S.%f')[:-4]
 
 s = socket.socket()
-s.connect(('10.12.110.197',8081))
+s.connect(('10.2.76.100',8081))
 str = "send"
 s.send(str.encode());
 s.close()
-print("Completed Exit")
+sys.stdout.write("Handover Completed ")
+print datetime.now().strftime('%H:%M:%S.%f')[:-4]
 exit()
