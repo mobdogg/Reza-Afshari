dcaedeedc2f88e69a98da8808829dc0752ca9306
mobdogg <58931842+mobdogg@users.noreply.github.com>
2020-06-10 20:05:43 +0700

    Update hostb.py

--- .github/workflows/hostb.py
+++ .github/workflows/hostb.py
@@ -3,6 +3,8 @@ import os
 from watchdog.observers import Observer 
 from watchdog.events import FileSystemEventHandler 
 import tarfile
+import sys
+from datetime import datetime 
   
   
 class OnMyWatch: 
@@ -18,7 +20,7 @@ class OnMyWatch:
         self.observer.start() 
         try: 
             while True: 
-                time.sleep(5) 
+                time.sleep(7) 
         except: 
             self.observer.stop() 
             print("Observer Stopped") 
@@ -34,8 +36,12 @@ class Handler(FileSystemEventHandler):
             return None 
         elif event.event_type == 'modified': 
             # Event is modified, you can process it now
+	    sys.stdout.write("Migration completed ")
+	    print datetime.now().strftime('%H:%M:%S.%f')[:-4]
 	    tf = tarfile.open("checkpoint1.tgz")
 	    tf.extractall()
+	    sys.stdout.write("Checkpoint Extracted ")
+	    print datetime.now().strftime('%H:%M:%S.%f')[:-4]
 	    os.system("python hostb2.py")
 	    exit()
               
