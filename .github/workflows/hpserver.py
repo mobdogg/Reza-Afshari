10be5f52452b7de6c22f839a64cd5e4c452ad49c
mobdogg <58931842+mobdogg@users.noreply.github.com>
2020-06-20 14:43:00 +0700

    Update hpserver.py

--- .github/workflows/hpserver.py
+++ .github/workflows/hpserver.py
@@ -4,24 +4,46 @@ import socket
 import time
 import os
 import sys
-from datetime import datetime
+import datetime
+
+class Timer(object):
+    """A simple timer class"""
+    
+    def __init__(self):
+        pass
+    
+    def start(self):
+        """Starts the timer"""
+        self.start = datetime.datetime.now()
+    
+    def stop(self):
+        """Stops the timer.  Returns the time elapsed"""
+        self.stop = datetime.datetime.now()
+        print str(self.stop - self.start)
+    def elapsed(self):
+        """Time elapsed since start was called"""
+        print str(datetime.datetime.now() - self.start)[:-3]
+    
+watch=Timer()
 
 listensocket = socket.socket()
 port=8080
 ip = socket.gethostname()
 listensocket.bind(('',port))
 listensocket.listen(5)
+print("Waiting for connection...")
 clientsocket, client_address = listensocket.accept()
 print("Connected")
 sys.stdout.write("Starting ")
-print datetime.now().strftime('%H:%M:%S.%f')[:-4]
+watch.start()
 #os.system("docker run -d --name looper busybox:latest \
          #/bin/sh -c 'i=0; while true; do echo $i; i=$(expr $i + 1); sleep 1; done'")
 running=True
 while running:
         message = clientsocket.recv(1024).decode()
     	if not message == "":
-	    fullData = "Connected"
+	    sys.stdout.write("Order received ")
+	    watch.elapsed()
 	    os.system("python hosta.py")
             time.sleep(0)
     	else:
