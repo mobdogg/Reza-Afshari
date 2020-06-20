2a253e82c348d91d16c2542d59f6ddaef9b784bf
mobdogg <58931842+mobdogg@users.noreply.github.com>
2020-06-20 14:41:20 +0700

    Update hostb.py

--- .github/workflows/hostb.py
+++ .github/workflows/hostb.py
@@ -1,45 +1,40 @@
+import time 
 import os
-import socket
+from watchdog.observers import Observer 
+from watchdog.events import FileSystemEventHandler 
 import sys
-import datetime
-import tarfile
+from datetime import datetime
+  
 
-class Timer(object):
-    """A simple timer class"""
-    
-    def __init__(self):
-        pass
-    
-    def start(self):
-        """Starts the timer"""
-        self.start = datetime.datetime.now()
-        print ("0:00:00.000")
-    
-    def stop(self):
-        """Stops the timer.  Returns the time elapsed"""
-        self.stop = datetime.datetime.now()
-        print str(self.stop - self.start)
-    def elapsed(self):
-        """Time elapsed since start was called"""
-        print str(datetime.datetime.now() - self.start)[:-3]
-watch=Timer()
-watch.start()
-tf = tarfile.open("checkpoint1.tgz")
-tf.extractall()
-sys.stdout.write("Checkpoint Extracted ")
-watch.elapsed()
-#os.system("docker create --name looper-clone oraclelinux:8.2 \ /bin/sh -c 'i=0; while true; do echo $i; i=$(expr $i + 1); sleep 1; done'")
-#sys.stdout.write("Container Created ")
-#watch.elapsed()
-os.system("docker start -p 192.168.43.184:80:80 -w=/app --checkpoint-dir=/migration/migration --checkpoint=checkpoint1 looper-clone")
-sys.stdout.write("Checkpoint Restored ")
-watch.elapsed()
+class OnMyWatch: 
+    # Set the directory on watch 
+    watchDirectory = "/migration"
+    def __init__(self): 
+        self.observer = Observer() 
+    def run(self): 
+        event_handler = Handler() 
+        self.observer.schedule(event_handler, self.watchDirectory, recursive = True) 
+        self.observer.start() 
+        try: 
+            while True: 
+                time.sleep(7) 
+        except: 
+            self.observer.stop() 
+            print("Observer Stopped") 
+        self.observer.join()
 
-s = socket.socket()
-s.connect(('10.33.211.110',8081))
-data = "send"
-s.send(data.encode());
-s.close()
-sys.stdout.write("Handover Completed ")
-watch.elapsed()
-exit()
+class Handler(FileSystemEventHandler): 
+ 
+    @staticmethod
+    def on_any_event(event): 
+        if event.is_directory: 
+            return None 
+        elif event.event_type == 'modified': 
+            # Event is modified, you can process it now
+	    os.system("python hostb2.py")
+	    exit()
+              
+  
+if __name__ == '__main__': 
+    watch = OnMyWatch() 
+watch.run() 
